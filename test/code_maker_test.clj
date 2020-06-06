ca324ac2aff5bdda98e930cc15c4d02ba6be2c9c
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 15:12:43 +0200

    shuffles the code and guess and compare the pairs. The result of that comparison will be reduced

--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -8,8 +8,8 @@
     "score guess with no matches"
     (score [0 0 0 0] [1 1 1 1]) => [0])
   (fact
-    "score guess with on :pos match"
+    "score guess one match"
     (score [0 0 0 0] [0 1 1 1]) => [1])
   (fact
-    "guess with two :pos matches"
+    "guess with two matches"
     (score [0 0 0 0] [0 1 1 0]) => [2]))
