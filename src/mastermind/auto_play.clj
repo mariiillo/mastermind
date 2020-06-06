607950e683b8c3287ecbad4f7f71700831f0474f
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 22:36:25 +0200

    complete code

--- src/mastermind/auto_play.clj
+++ src/mastermind/auto_play.clj
@@ -4,15 +4,47 @@
     [mastermind.code-maker :as cm]))
 
 (defn random-code []
+  (cb/number-to-guess (rand-int (dec (* 5 5 5 5))))
   )
 
-(defn auto-play []
-  (let [code (random-code)]
-    (loop [n 1 past-scores [] last-guess nil]
-      (let [guess (cb/break-code-seq last-guess past-scores)
-            score (cm/score code guess)]
-        (if (= score [4 0])
-          n
-          (recur (inc n) (conj past-scores [guess score]) guess))
-        )))
+(defn auto-play
+  ([]
+   (auto-play cb/break-code-seq))
+  ([strategy]
+   (let [code (random-code)]
+     (loop [n 1 past-scores [] starting-guess nil]
+       (let [guess (strategy starting-guess past-scores)
+             score (cm/score code guess)]
+         (if (= score [4 0])
+           n
+           (recur (inc n) (conj past-scores [guess score]) (cb/inc-guess guess)))
+         )
+       )
+     )
+   )
   )
+
+(def square #(* % %))
+
+(defn mean [x]
+  (/ (reduce + x) (count x)))
+
+(defn sigma [x]
+  (let [mn (mean x)]
+    (Math/sqrt
+      (/ (reduce #(+ %1 (square (- %2 mn))) 0 x)
+         (dec (count x))))))
+
+(defn analyze-strategy [strategy n]
+  (let [scores (sort (for [x (repeat n nil)] (auto-play strategy)))]
+    {:mean (double (mean scores))
+     :sigma (sigma scores)
+     :min (first scores)
+     :max (last scores)
+     :median (nth scores (int (/ (count scores) 2)))
+     :hist (map count (partition-by identity scores))}))
+
+(defn analyze-strategies [n]
+  {:seq (analyze-strategy cb/break-code-seq n)
+   :3x2 (analyze-strategy cb/break-code-3x2 n)
+   :double-rainbow (analyze-strategy cb/break-code-double-rainbow n)})
