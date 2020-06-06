2740363ee32d270e58e8ddfacbd9ab1ac583846d
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 14:59:49 +0200

    return the number of :pos instead of a list with the :pos

--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -6,10 +6,10 @@
   "Code maker"
   (fact
     "score guess with no matches"
-    (score [0 0 0 0] [1 1 1 1]) => [])
+    (score [0 0 0 0] [1 1 1 1]) => [0])
   (fact
     "score guess with on :pos match"
-    (score [0 0 0 0] [0 1 1 1]) => [:pos])
+    (score [0 0 0 0] [0 1 1 1]) => [1])
   (fact
     "guess with two :pos matches"
-    (score [0 0 0 0] [0 1 1 0]) => [:pos :pos]))
+    (score [0 0 0 0] [0 1 1 0]) => [2]))
