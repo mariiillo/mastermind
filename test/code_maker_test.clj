6aa5df16fabe3ceda07a2e16529af7c414b1df03
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 15:41:01 +0200

    add comment in test add more tests for value matches

--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -21,8 +21,12 @@
 )
 
 (facts
-  "Scoring Value Matches"
+  "Scoring Value Matches. Matches that have the right value, but are in the wrong position."
 
   (fact
-    "one value match"
-    (score [1 2 3 4] [2 0 0 0]) => [0 1]))
+    "value matches"
+    (score [1 2 3 4] [2 0 0 0]) => [0 1]
+    (score [1 2 3 4] [2 3 0 0]) => [0 2]
+    (score [1 2 3 4] [2 4 1 0]) => [0 3]
+    (score [1 2 3 4] [4 3 2 1]) => [0 4]
+    (score [1 2 3 4] [2 3 4 1]) => [0 4]))
