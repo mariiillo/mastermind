48b27c385cd28481403896e111b616c40eafa93a
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 15:22:24 +0200

    modify tests to expect number of right digits and the number of digits that are right but not in the right position.

--- test/code_maker_test.clj
+++ test/code_maker_test.clj
@@ -6,16 +6,16 @@
   "Code maker"
   (fact
     "score guess with no matches"
-    (score [0 0 0 0] [1 1 1 1]) => [0])
+    (score [0 0 0 0] [1 1 1 1]) => [0 0])
   (fact
     "score guess one match"
-    (score [0 0 0 0] [0 1 1 1]) => [1])
+    (score [0 0 0 0] [0 1 1 1]) => [1 0])
   (fact
     "guess with two matches"
-    (score [0 0 0 0] [0 1 1 0]) => [2])
+    (score [0 0 0 0] [0 1 1 0]) => [2 0])
   (fact
     "guess with many position matches"
-    (score [1 1 1 1] [0 1 1 1]) => [3]
-    (score [0 0 0 0] [0 0 0 1]) => [3]
-    (score [1 2 3 4] [1 2 3 4]) => [4])
+    (score [1 1 1 1] [0 1 1 1]) => [3 0]
+    (score [0 0 0 0] [0 0 0 1]) => [3 0]
+    (score [1 2 3 4] [1 2 3 4]) => [4 0])
 )
