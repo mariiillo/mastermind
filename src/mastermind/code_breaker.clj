ba2486c0aece9d3e850f18b09e5d93c022ca6a89
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-06 20:13:29 +0200

    add number-to-guess

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -1,4 +1,14 @@
 (ns mastermind.code-breaker)
 
+(defn guess-to-number [guess]
+  (reduce #(+ (* 6 %1) %2) guess))
+
+(defn number-to-guess [number]
+  [(rem (quot number 216) 6)
+   (rem (quot number 36) 6)
+   (rem (quot number 6) 6)
+   (rem number 6)]
+  )
+
 (defn break-code [past-guesses]
   [0 0 0 0])
--- test/mastermind/code_breaker_test.clj
+++ test/mastermind/code_breaker_test.clj
@@ -2,6 +2,38 @@
   (:require [midje.sweet :refer :all]
             [mastermind.code-breaker :refer :all]))
 
-(facts "Code Breaker"
-      (fact "initial guess"
-            (break-code []) => [0 0 0 0]))
+(facts
+  "Code Breaker"
+  (fact
+    "guess-to-number"
+    (guess-to-number [0 0 0 0]) => 0
+    (guess-to-number [0 0 0 1]) => 1
+    (guess-to-number [0 0 1 0]) => 6
+    (guess-to-number [0 0 1 1]) => 7
+    (guess-to-number [0 1 1 1]) => 43
+    (guess-to-number [1 1 1 1]) => 259
+    (guess-to-number [5 5 5 5]) => (dec (* 6 6 6 6)))
+
+  (fact
+    "number-to-guess"
+    (number-to-guess 0 ) => [0 0 0 0]
+    (number-to-guess 1 ) => [0 0 0 1]
+    (number-to-guess 6 ) => [0 0 1 0]
+    (number-to-guess 7 ) => [0 0 1 1]
+    (number-to-guess 43 ) => [0 1 1 1]
+    (number-to-guess 259 ) => [1 1 1 1]
+    (number-to-guess (dec (* 6 6 6 6)) ) => [5 5 5 5]
+    )
+
+  (future-fact
+    "increment guess"
+     (inc-guess [0 0 0 0]) => [0 0 0 1])
+
+   (fact
+     "initial guess"
+     (break-code []) => [0 0 0 0])
+
+   (future-fact
+     "Walk through solution of code [1 2 3 4]"
+     (break-code [[[0 0 0 0]
+                   (cm/score [1 2 3 4] [0 0 0 0])]]) => [0 0 0 1]))
